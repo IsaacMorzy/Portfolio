27fab891b24ed2c4d9c8f3a0e7f4b418b651062a
IsaacMorzy <musyokaisaac98@gmail.com>
2019-11-17 15:16:18 +0300

    add proects app

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,7 @@ LOCAL_APPS = [
     # Your stuff: custom apps go here
     "projects",
     "contents",
-    "blog"
+    "blog",
 
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
